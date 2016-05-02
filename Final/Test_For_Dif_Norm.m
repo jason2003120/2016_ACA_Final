d96f7a8ad7b738676b18f579d577572da01bf04c
jason2003120 <jason2003120@gmail.com>
2016-05-02 18:30:44 -0400

    update

--- Final/Test_For_Dif_Norm.m
+++ Final/Test_For_Dif_Norm.m
@@ -1,41 +1,41 @@
 clear all;
 %%
 start_time_1 = 0;
-stop_time_1 = 1200;
+stop_time_1 = 600;
 start_time_2 = 0;
-stop_time_2 = 1200;
-acoustic_params = struct('win_size',1024,'hop_size',512,'min_freq',40,'max_freq',6000,...
+stop_time_2 = 600;
+acoustic_params = struct('win_size',512,'hop_size',256,'min_freq',40,'max_freq',6000,...
                 'num_mel_filts',26,'ITW',1);
 % function [ data_u,data_s ] = get_feature_acoustic( fpath, mfsc_params,label,start_time, stop_time )
 % male sound - label = 1
 
 %%
-fpath1 = 'C:\Users\User\Desktop\audioset\male_004.wav';
-fpath2 = 'C:\Users\User\Desktop\audioset\female_001.wav';
+fpath1 = 'C:\Users\User\Desktop\audioset\man_001_new.wav';
+fpath2 = 'C:\Users\User\Desktop\audioset\women_001_new.wav';
 %%
 
-[data1] = get_feature_acoustic_SumtoOne(fpath1,acoustic_params,1,start_time_1,stop_time_1);
-
-% female sound - label = 2
-[data2]  = get_feature_acoustic_SumtoOne(fpath2,acoustic_params,2, start_time_2 ,stop_time_2);
-
-% create arff
-
-data = [data1 ; data2];
-fname = 'train_feature_4_1_STO';
-arffwrite(fname,data,acoustic_params);
-
-
-[data1] = get_feature_acoustic_Max(fpath1,acoustic_params,1,start_time_1,stop_time_1);
-
-% female sound - label = 2
-[data2]  = get_feature_acoustic_Max(fpath2,acoustic_params,2, start_time_2 ,stop_time_2);
-
-% create arff
-
-data = [data1 ; data2];
-fname = 'train_feature_4_1_MAX';
-arffwrite(fname,data,acoustic_params);
+% [data1] = get_feature_acoustic_SumtoOne(fpath1,acoustic_params,1,start_time_1,stop_time_1);
+% 
+% % female sound - label = 2
+% [data2]  = get_feature_acoustic_SumtoOne(fpath2,acoustic_params,2, start_time_2 ,stop_time_2);
+% 
+% % create arff
+% 
+% data = [data1 ; data2];
+% fname = 'train_feature_4_1_STO';
+% arffwrite(fname,data,acoustic_params);
+% 
+% 
+% [data1] = get_feature_acoustic_Max(fpath1,acoustic_params,1,start_time_1,stop_time_1);
+% 
+% % female sound - label = 2
+% [data2]  = get_feature_acoustic_Max(fpath2,acoustic_params,2, start_time_2 ,stop_time_2);
+% 
+% % create arff
+% 
+% data = [data1 ; data2];
+% fname = 'train_feature_4_1_MAX';
+% arffwrite(fname,data,acoustic_params);
 
 
 [data1] = get_feature_acoustic_MinNMax(fpath1,acoustic_params,1,start_time_1,stop_time_1);
@@ -46,36 +46,40 @@ arffwrite(fname,data,acoustic_params);
 % create arff
 
 data = [data1 ; data2];
-fname = 'train_feature_4_1_MNM';
+fname = 'train_feature_001';
 arffwrite(fname,data,acoustic_params);
+%% 
 
-%%
-fpath1 = 'C:\Users\User\Desktop\audioset\male_005.wav';
-fpath2 = 'C:\Users\User\Desktop\audioset\female_005.wav';
+% for testing data
+fpath1 = 'C:\Users\User\Desktop\audioset\man_002_new.wav';
+fpath2 = 'C:\Users\User\Desktop\audioset\women_002_new.wav';
 
-
-[data1] = get_feature_acoustic_SumtoOne(fpath1,acoustic_params,1,start_time_1,stop_time_1);
-
-% female sound - label = 2
-[data2]  = get_feature_acoustic_SumtoOne(fpath2,acoustic_params,2, start_time_2 ,stop_time_2);
-
-% create arff
-
-data = [data1 ; data2];
-fname = 'train_feature_5_5_STO';
-arffwrite(fname,data,acoustic_params);
-
-
-[data1] = get_feature_acoustic_Max(fpath1,acoustic_params,1,start_time_1,stop_time_1);
-
-% female sound - label = 2
-[data2]  = get_feature_acoustic_Max(fpath2,acoustic_params,2, start_time_2 ,stop_time_2);
-
-% create arff
-
-data = [data1 ; data2];
-fname = 'train_feature_5_5_MAX';
-arffwrite(fname,data,acoustic_params);
+start_time_1 = 0;
+stop_time_1 = 100;
+start_time_2 = 0;
+stop_time_2 = 100;
+% [data1] = get_feature_acoustic_SumtoOne(fpath1,acoustic_params,1,start_time_1,stop_time_1);
+% 
+% % female sound - label = 2
+% [data2]  = get_feature_acoustic_SumtoOne(fpath2,acoustic_params,2, start_time_2 ,stop_time_2);
+% 
+% % create arff
+% 
+% data = [data1 ; data2];
+% fname = 'train_feature_5_5_STO';
+% arffwrite(fname,data,acoustic_params);
+% 
+% 
+% [data1] = get_feature_acoustic_Max(fpath1,acoustic_params,1,start_time_1,stop_time_1);
+% 
+% % female sound - label = 2
+% [data2]  = get_feature_acoustic_Max(fpath2,acoustic_params,2, start_time_2 ,stop_time_2);
+% 
+% % create arff
+% 
+% data = [data1 ; data2];
+% fname = 'train_feature_5_5_MAX';
+% arffwrite(fname,data,acoustic_params);
 
 
 [data1] = get_feature_acoustic_MinNMax(fpath1,acoustic_params,1,start_time_1,stop_time_1);
@@ -84,11 +88,9 @@ arffwrite(fname,data,acoustic_params);
 [data2]  = get_feature_acoustic_MinNMax(fpath2,acoustic_params,2, start_time_2 ,stop_time_2);
 
 % create arff
-
 data = [data1 ; data2];
-fname = 'train_feature_5_5_MNM';
+fname = 'test_feature_001';
 arffwrite(fname,data,acoustic_params);
-
 fprintf('ok!!!');
 
 
